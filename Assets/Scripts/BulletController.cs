8789067f3a2c9676ef21f7a2c156930186787331
Malte <38046577+ColossusDev@users.noreply.github.com>
2019-11-21 20:50:53 +0100

    Version 1.121
    Kugeln eingebaut
    Kugeln fliegen in die Richtung der Maus
    keine Kollision
    noch Probleme mit gleichmäßiger Geschwindigkeit

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    GameObject player;
+
+    Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+
+        direction = player.GetComponent<PlayerController>().getDirection();
+
+        Debug.Log("X" + direction.x);
+        Debug.Log("Y" + direction.y);
+        Debug.Log("Z" + direction.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = transform.position + direction * Time.deltaTime;
+
+        transform.position = move;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Destroy(this.gameObject);
     }
 }
