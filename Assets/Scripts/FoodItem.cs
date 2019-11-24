10a9063b4e03d850846454796e7c6b205d41be08
Malte <38046577+ColossusDev@users.noreply.github.com>
2019-11-24 03:52:18 +0100

    Version 1.36
    - Item verbrauchen möglich
    - Particel Effect noch in Arbeit (sollte in eingenen Test Branch ausgelagert werden)

---
+++ Assets/Scripts/FoodItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : MonoBehaviour
+{
+    public GameObject effect;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+    }
+
+    public void Use()
+    {
+        Instantiate(effect, player.position, Quaternion.identity);
+        //Später hier noch Heal und Food hinzufügen
+        Destroy(gameObject);
+    }
+}
